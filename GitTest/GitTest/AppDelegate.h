d51345536dc504bffcaef6f50a3722e5217910a8
Flying <flying@mac.padavan>
2018-11-28 22:33:42 +0800

    初始化项目

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Flying on 2018/11/28.
+//  Copyright © 2018年 Flying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Flying on 2018/11/28.
+//  Copyright © 2018年 Flying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
