ac2624d32d857686ef7ef721db06d1f9ab35142f
李鹏飞 <iamflying@vip.qq.com>
2018-11-29 10:18:20 +0800

    11111

---
+++ GitTest/GitTest/UIView3.h
@@ -0,0 +1,15 @@
+//
+//  UIView3.h
+//
+//  Created by LPF. on 2018/11/29.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView3 : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GitTest/GitTest/UIView3.m
@@ -0,0 +1,19 @@
+//
+//  UIView3.m
+//
+//  Created by LPF. on 2018/11/29.
+//
+
+#import "UIView3.h"
+
+@implementation UIView3
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
